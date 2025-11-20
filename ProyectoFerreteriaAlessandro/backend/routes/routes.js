dc289518838c3a14283b1da02df36011d90fa1eb
Kenny5231 <124184507+Kenny5231@users.noreply.github.com>
2025-11-20 00:21:29 -0600

    Creacion de rutas de sucursal
    Se crearon las rutas mas importantes con respecto a las sucursales y se preparo todo para el usuario

--- ProyectoFerreteriaAlessandro/backend/routes/routes.js
+++ ProyectoFerreteriaAlessandro/backend/routes/routes.js
@@ -22,14 +22,17 @@ router.get('/test', (req, res) => {
 
 // Importar rutas de módulos específicos
 const rolesRoutes = require('./rol.routes');
-// const usuariosRoutes = require('./usuarios.routes');
+const sucursalesRoutes = require('./sucursal.routes');
+const usuariosRoutes = require('./usuario.routes');
 // const productosRoutes = require('./productos.routes');
 // const ventasRoutes = require('./ventas.routes');
 // const comprasRoutes = require('./compras.routes');
 
 // Montar rutas
 router.use('/roles', rolesRoutes);
-// router.use('/usuarios', usuariosRoutes);
+router.use('/sucursales', sucursalesRoutes);
+router.use('/usuarios', usuariosRoutes);
+// router.use('/productos', productosRoutes);
 // router.use('/productos', productosRoutes);
 // router.use('/ventas', ventasRoutes);
 // router.use('/compras', comprasRoutes);
---
+++ ProyectoFerreteriaAlessandro/backend/services/usuario.service.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+
+// TODO: Implementar funciones del service
+// const getAllUsuarios = async () => {};
+// const getUsuarioById = async (id) => {};
+// const createUsuario = async (data) => {};
+// const updateUsuario = async (id, data) => {};
+// const deleteUsuario = async (id) => {};
+
+module.exports = {
+  // getAllUsuarios,
+  // getUsuarioById,
+  // createUsuario,
+  // updateUsuario,
+  // deleteUsuario
+};
